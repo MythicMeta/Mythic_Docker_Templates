cbe5629fa3f769a54f6a07621cb99c84880b82da
Cody Thomas <codybthomas@gmail.com>
2021-12-02 18:25:39 -0800

    updated files for mythic 2.3

--- Docker_Payload_Type_base_files/create_base_images.sh
+++ Docker_Payload_Type_base_files/create_base_images.sh
@@ -11,6 +11,6 @@ docker build -f "python38_dockerfile" --rm -t "python38_payload" .
 echo "Building golden xgo image..."
 docker build -f "xgolang_dockerfile" --rm -t "xgolang_payload" .
 
-#docker image prune -f
+docker image prune -f
 echo "Building golden leviathan image..."
 docker build -f "leviathan_dockerfile" --rm -t "leviathan_payload" .
