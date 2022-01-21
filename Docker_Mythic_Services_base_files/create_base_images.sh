8c7edd405cfb64713a6e9a602607f743d5270eb1
its-a-feature <codybthomas@gmail.com>
2022-01-21 15:02:06 -0800

    updating the requirements and mythic server for latex

--- Docker_Mythic_Services_base_files/create_base_images.sh
+++ Docker_Mythic_Services_base_files/create_base_images.sh
@@ -1,11 +1,11 @@
 echo "Pruning old images..."
 docker image prune -f
 echo "Building golden postgres image..."
-#docker build -f "mythic_postgres" --rm -t "mythic_postgres" .
+docker build -f "mythic_postgres" --rm -t "mythic_postgres" .
 
 docker image prune -f
 echo "Building golden rabbitmq image..."
-#docker build -f "mythic_rabbitmq" --rm -t "mythic_rabbitmq" .
+docker build -f "mythic_rabbitmq" --rm -t "mythic_rabbitmq" .
 
 docker image prune -f
 echo "Building golden server image..."
